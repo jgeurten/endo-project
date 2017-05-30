9492cf12301ad8b22d2bc6b540b0687b9aec11a4
Jordan Geurten <jordangeurten@gmail.com>
2017-05-30 17:20:13 -0400

    Removed debuging lines

--- EndoScannerArduinoFirmware/EndoScannerArduinoFirmware.ino
+++ EndoScannerArduinoFirmware/EndoScannerArduinoFirmware.ino
@@ -2,7 +2,9 @@
 #include "configuration.h"
 #include "GCodeInterpreter.h"
 
- char serialMsg[MAXBYTES + 1]; //extra block of memory for '\0\'
+char serialMsg[MAXBYTES + 1]; //extra block of memory for '\0\'
+int count = 0;
+char first[MAXBYTES + 1];
 
 void setup() {
   initializeLaser();   // sets baud rate as well.
@@ -11,17 +13,14 @@ void setup() {
 
 void loop()
 {
-  while (Serial.available() > 0)
+  if (Serial.available() > 0)
   {
     delay(100);
-   
     for (int numBytes = 0; numBytes < MAXBYTES; numBytes++) {
-
       serialMsg[numBytes] = Serial.read();
       serialMsg[numBytes + 1] = '\0';    //serialMsg[1] = '\0'
     }
-    //Serial.print(serialMsg);
-    processCode(serialMsg);
+    processCode(serialMsg); 
   }
 }
 
