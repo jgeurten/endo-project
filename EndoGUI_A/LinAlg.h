46fe9552d7e912ffd6d89d46570367cab805d40d
Jordan Geurten <jordangeurten@gmail.com>
2017-06-23 14:26:45 -0400

    Added laser line and camera line intersection method

---
+++ EndoGUI_A/LinAlg.h
@@ -0,0 +1,93 @@
+#pragma once
+#ifndef LINALG_H
+#define LINALG_H
+
+
+// structures:
+
+typedef struct _EndoPt
+{
+	double x;
+	double y;
+	double z;
+
+} EndoPt;
+
+typedef struct _EndoLine	//really composed of two 'points'
+{
+	EndoPt a;
+	EndoPt b;
+
+} EndoLine;
+
+
+
+static EndoPt MakePoint(double x, double y, double z)
+{
+	EndoPt pt;
+	pt.x = x;
+	pt.y = y;
+	pt.z = z;
+	return pt;
+}
+
+static EndoLine MakeLine(EndoLine a, EndoPt b)
+{
+	EndoLine line;
+	line.a.x = a.x;
+	line.a.y = a.y;
+	line.a.z = a.z;
+
+	line.b.x = b.x;
+	line.b.y = b.y;
+	line.b.z = b.z;
+	return line;
+}
+
+
+static EndoLine lineFromPoints(EndoPt p1, EndoPt p2)
+{
+	EndoPt b;
+	b.x = (p2.x - p1.x);
+	b.y = (p2.y - p1.y);
+	b.x = (p2.z - p1.z);
+
+	EndoLine line = MakeLine(p1, b);
+	return line;
+}
+
+EndoPt CrossProduct(EndoPt p1, EndoPt p2)
+{
+	EndoPt result;
+	result.x = p1.y*p2.z - (p1.z*p2.y);
+	result.y = p1.x*p2.z - (p2.x*p1.z);
+	result.z = p1.x*p2.y - (p2.x*p1.y);
+	return result;
+}
+
+EndoPt intersectionOfLines(EndoLine l1, EndoLine l2)
+{
+	double t;
+	EndoPt bcross, across, adelta, result;
+	bcross = CrossProduct(l1.b, l2.b);
+	if (bcross.x == 0 && bcross.y == 0 && bcross.z == 0)
+		return;
+
+	adelta.x = l2.x - l1.x;
+	adelta.y = l2.y - l1.y;
+	adelta.z = l2.z - l1.z;
+
+	across = CrossProduct(adelta, ls.b);
+
+	//may have to be range .. unsure about precision of tracking
+	if (across.x / bcross.x != across.y / bcross.y || across.x / bcross.x != across.z / bcross.z)
+		return;
+
+	t = across.x / bcross.x;
+	result.x = l1.a.x + t*l1.b.x;
+	result.y = l1.a.y + t*l1.b.y;
+	result.z = l1.a.z + t*l1.b.z;
+	return result;
+}
+
+#endif // !LINALG_H
